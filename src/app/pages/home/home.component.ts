8f6190309e89ec276f4d2a4d90f7e502fa5da471
Karxem <tippjoshua@gmx.de>
2023-11-06 22:06:23 +0100

    feat(components): created input component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BingoGridComponent } from 'src/app/components/bingo-grid/bingo-grid.component';
-import { SpeedDialComponent } from 'src/app/components/speed-dial/speed-dial.component';
+import { InputComponent } from 'src/app/components/input/input.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
-  imports: [CommonModule, BingoGridComponent, SpeedDialComponent]
+  imports: [CommonModule, InputComponent]
 })
 export class HomeComponent {
 
