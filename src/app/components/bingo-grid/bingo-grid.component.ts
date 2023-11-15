d307c41d593504d558f7983ffc05b31b365dde2f
Joshua Tippelt <joshua.tippelt@brainbits.net>
2023-11-15 14:22:25 +0100

    feat: add color support to bingo-grid component

--- src/app/components/bingo-grid/bingo-grid.component.ts
+++ src/app/components/bingo-grid/bingo-grid.component.ts
@@ -1,26 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Word } from 'src/app/word.model';
-import { WordsService } from 'src/app/words.service';
-import { WinComponent } from '../win/win.component';
+import { Component, Input, OnInit } from '@angular/core';
+import { Word } from '../../shared/word.model';
+import { WordsService } from '../../shared/words.service';
+import { StorageService } from 'src/app/shared/storage.service';
+import { Color } from 'src/app/shared/color.model';
 
 @Component({
   selector: 'app-bingo-grid',
   templateUrl: './bingo-grid.component.html',
   standalone: true,
-  imports: [CommonModule, WinComponent]
+  imports: [CommonModule],
 })
 export class BingoGridComponent implements OnInit {
+  @Input({ required: true }) color: Color = 'default';
+  private key: string = 'grid';
   public bingoGrid: Word[][] = [];
   public bingoText: string = '';
   public isBingo: boolean = false;
 
-  constructor(private wordsService: WordsService) {}
+  constructor(
+    private wordsService: WordsService,
+    private storageService: StorageService
+  ) {}
 
   ngOnInit(): void {
-    this.wordsService.getWords().subscribe((words) => {
-      this.generateBingoGrid(words);
-    });
+    if (this.storageService.getItem(this.key)) {
+      this.bingoGrid = this.storageService.getItem(
+        'grid'
+      ) as unknown as Word[][];
+    } else {
+      this.wordsService.getWords().subscribe((words) => {
+        this.generateBingoGrid(words);
+
+        if (this.bingoGrid && !this.storageService.getItem(this.key)) {
+          this.storageService.setItem(this.key, this.bingoGrid);
+          console.log('Grid set: ' + this.storageService.getItem(this.key));
+        }
+      });
+    }
+
+    if (!this.storageService.getItem('activeColor')) {
+      this.storageService.setItem('activeColor', 'default');
+    }
+
+    this.color = this.storageService.getItem('activeColor') as Color;
+    console.log(this.color);
   }
 
   generateBingoGrid(words: Word[]): void {
@@ -42,6 +66,11 @@ export class BingoGridComponent implements OnInit {
   }
 
   toggleCell(word: Word): void {
+    // Block all when there is a bingo
+    if (this.isBingo) {
+      return;
+    }
+
     word.clicked = !word.clicked;
 
     // Check rows for bingo
@@ -49,10 +78,12 @@ export class BingoGridComponent implements OnInit {
       if (this.bingoGrid[i].every((word) => word.clicked)) {
         this.bingoText = 'Bingo in Zeile ' + (i + 1);
         this.isBingo = true;
+        this.storageService.setItem('isBingo', true);
         return;
       }
 
       this.isBingo;
+      this.storageService.setItem('grid', this.bingoGrid);
     }
 
     // Check columns for bingo
