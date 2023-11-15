654b2bba3a85d767677fbbe8b83926523cd080af
Joshua Tippelt <joshua.tippelt@brainbits.net>
2023-11-15 14:23:51 +0100

    refactor: activate color support through components

--- src/app/pages/bingo/bingo.component.ts
+++ src/app/pages/bingo/bingo.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BingoGridComponent } from 'src/app/components/bingo-grid/bingo-grid.component';
-import { SpeedDialComponent } from 'src/app/components/speed-dial/speed-dial.component';
+import { DrawerComponent } from 'src/app/components/drawer/drawer.component';
+import { Color } from 'src/app/shared/color.model';
+import { StorageService } from 'src/app/shared/storage.service';
 
 @Component({
   selector: 'app-bingo',
   templateUrl: './bingo.component.html',
   standalone: true,
-  imports: [CommonModule, BingoGridComponent, SpeedDialComponent]
+  imports: [CommonModule, BingoGridComponent, DrawerComponent]
 })
 export class BingoComponent {
+  public activeColor: Color = 'default';
 
+  constructor(private storageService: StorageService) {}
+
+  public changeColor(color: string):void {
+    this.activeColor = color as Color;
+
+    this.storageService.setItem('activeColor', color)
+  }
 }
