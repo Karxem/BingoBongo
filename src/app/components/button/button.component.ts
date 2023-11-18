43003c240097ae4a8bf2a161ca5147e5d2a42831
Karxem <tippjoshua@gmx.de>
2023-11-18 13:05:07 +0100

    feat: add styling to button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Color } from 'src/app/shared/color.model';
+import { StorageService } from 'src/app/shared/storage.service';
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
+  @Input({ required: true }) label: string = 'Button';
+  public activeColor: Color = 'default';
 
+  constructor(private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    this.activeColor = this.storageService.getItem('activeColor') as Color;
+  }
 }
