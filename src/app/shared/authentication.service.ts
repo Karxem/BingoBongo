b16790924e941be53297fb2c9953f0aab2d72a24
Karxem <tippjoshua@gmx.de>
2023-11-19 23:43:22 +0100

    feat: create authentication service

---
+++ src/app/shared/authentication.service.ts
@@ -0,0 +1,55 @@
+// authentication.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { Login } from './login.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  private loggedIn: Login = { isLoggedIn: false, username: '' };
+  apiUrl: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const userData = { username, password };
+    return this.http.post<boolean>(`${this.apiUrl}/login`, userData).pipe(
+      tap((success: boolean) => {
+        this.loggedIn.isLoggedIn = success; // Log in on success
+        this.loggedIn.username = username;
+      }),
+      catchError((error) => {
+        console.error('Error during login', error);
+        return of(false);
+      })
+    );
+  }
+
+  register(username: string, password: string): Observable<any> {
+    const registerData = { username, password };
+    return this.http.post<any>(`${this.apiUrl}/register`, registerData).pipe(
+      tap((response) => {
+        if (response.success) {
+          this.loggedIn.isLoggedIn = true; // Automatically log in after registration
+          this.loggedIn.username = username;
+        }
+      }),
+      catchError((error) => {
+        console.error('Error during registration', error);
+        return of({ success: false, message: 'Registration failed' });
+      })
+    );
+  }
+
+  logout(): void {
+    this.loggedIn.isLoggedIn = false;
+    console.log('Logout');
+  }
+
+  isLoggedIn(): Login {
+    return this.loggedIn || !!sessionStorage.getItem('token');
+  }
+}
